2be1398981956abe15b053e2cc0dedad6b530c7f
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-01-14 15:50:29 +0100

    adding default routes reply

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,4 +3,15 @@ const country=require('./country.routes');
 
 router.use('/country',country);
 
+const defaultReply=(req,res)=>{
+    res.status(404);
+    res.send('Please read documentation')
+}
+
+router.get('/',defaultReply);
+router.get('/:id',defaultReply);
+router.post('/',defaultReply);
+router.put('/:id',defaultReply);
+router.delete('/:id',defaultReply);
+
 module.exports=router;
