feb6ddea1e569a44dbae476302e65aa904eeca40
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-01-30 22:02:51 +0100

    getting all users OK

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,46 +1,54 @@
-const userModel=require('../models/users.model');
-const User=require('../classes/User.class');
-const Joi=require('joi');
-const {encrypt}=require('../utils/crypto');
+const userModel = require('../models/users.model');
+const User = require('../classes/User.class');
+const Joi = require('joi');
+const { encrypt } = require('../utils/crypto');
 
-const  validate=(data, forCreation = true)=>{
+const validate = (data, forCreation = true) => {
     const presence = forCreation ? 'required' : 'optional';
     return Joi.object({
-        firstname : Joi.string().max(200).presence(presence),
-        lastname : Joi.string().max(200).presence(presence),
-        password : Joi.string().max(200).presence(presence),
-        login : Joi.string().max(200).presence(presence),
-        email : Joi.string().email().presence(presence),
-        rank : Joi.number().integer().min(1).max(8).presence('optional')
-    }).validate(data,{abortEarly:false}).error;
+        firstname: Joi.string().max(200).presence(presence),
+        lastname: Joi.string().max(200).presence(presence),
+        password: Joi.string().max(200).presence(presence),
+        login: Joi.string().max(200).presence(presence),
+        email: Joi.string().email().presence(presence),
+        rank: Joi.number().integer().min(1).max(8).presence('optional')
+    }).validate(data, { abortEarly: false }).error;
 }
 
 
-const getAll=async(req,res)=>{
-    const result=userModel;
+const getAll = async (req, res) => {
+    const result = await userModel.findAll();
+    if (result && result !== -1)
+        res.json(result);
+    else if (result === -1) {
+        res.sendStatus(500)
+    }
+    else {
+        res.sendStatus(404)
+    }
 }
 
-const getOne=async(req,res)=>{
-    const result=userModel;
+const getOne = async (req, res) => {
+    const result = userModel;
 }
 
-const addOne =async(req,res)=>{
+const addOne = async (req, res) => {
     //Check entry
-    const errors=validate(req.body);
-    if (errors){
-        const error=errors.details[0].message;
+    const errors = validate(req.body);
+    if (errors) {
+        const error = errors.details[0].message;
         return res.status(422).send(error);
     }
 
-    let {rank}=req.body;
-    if (!rank){
-        rank=1;
+    let { rank } = req.body;
+    if (!rank) {
+        rank = 1;
     }
 
-    const {password,firstname,lastname,email,login}=req.body;
-    let encryptedPassword=await encrypt( password);
+    const { password, firstname, lastname, email, login } = req.body;
+    let encryptedPassword = await encrypt(password);
 
-    const payload=new User(
+    const payload = new User(
         firstname,
         lastname,
         login,
@@ -48,27 +56,27 @@ const addOne =async(req,res)=>{
         rank,
         email
     )
-    const result=await userModel.addUser(payload);
-    if(result){
+    const result = await userModel.addUser(payload);
+    if (result) {
         res.json(result);
     }
-    else{
+    else {
         res.sendStatus(500)
     }
 }
 
-const updateUser=async(req,res)=>{
-     //Check entry
-    const result=userModel;
+const updateUser = async (req, res) => {
+    //Check entry
+    const result = userModel;
 }
 
-const deleteUser=async(req,res)=>{
-     //Check entry
-    const result=userModel;
+const deleteUser = async (req, res) => {
+    //Check entry
+    const result = userModel;
 
 }
 
-module.exports={
+module.exports = {
     getAll,
     getOne,
     addOne,
--- models/users.model.js
+++ models/users.model.js
@@ -1,6 +1,24 @@
 const { dbquery } = require('../utils/dbutils');
 const User=require('../classes/User.class');
 
+const findAll=async()=>{
+    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, email FROM user');
+    if (dbResult && dbResult !== -1) {
+        const resultat = dbResult.map(element => {
+            const item = new User(element.firstname,element.lastname,element.login,null,element.rankUser,element.email,element.id);
+            return item;
+        });
+        return resultat;
+    }
+    else if(dbResult===-1)
+    {
+        return undefined;
+    }
+    else
+        return -1;
+
+}
+
 const addUser=async(user)=>{
     
     const paramsArray=Object.values(user);
@@ -16,5 +34,6 @@ const addUser=async(user)=>{
 }
 
 module.exports={
-    addUser
+    addUser,
+    findAll,
 }
