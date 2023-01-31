f2538c43d9da9b5cdade229832d457cdbd4cb15d
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-01-31 21:21:48 +0100

    adding new method in comments for cors origin

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -6,3 +6,17 @@ const corsOptions ={
   }
 
   module.exports=corsOptions;
+
+  //Another way :
+
+ /*
+ //Should be an array of allowad sites, just to change method to test origin
+  corsOptions={
+    origin:(origin,callback)=>{
+      if(origin==='http://192.168.1.20:3000')
+        callback(null,true)
+      else
+        callback(new Error('Not allowed'));
+    },
+    optionSuccessStatus:200,
+  }*/
