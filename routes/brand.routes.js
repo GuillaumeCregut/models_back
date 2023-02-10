50e967864071ecf82b47abd6c91e13fe4e10b630
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-10 18:10:09 +0100

    adding route to index and link route with controller

--- routes/brand.routes.js
+++ routes/brand.routes.js
@@ -1,5 +1,11 @@
 const router =require('express').Router();
+const brandController=require('../controllers/brand.controller');
+const {userCheck}=require('../middlewares/UserValidation');
 
-
+router.get('/',brandController.getAll);
+router.get('/:id',brandController.getOne);
+router.post('/',brandController.addOne);
+router.put('/:id',brandController.updateOne);
+router.delete('/:id',brandController.deleteOne);
 
 module.exports=router;
