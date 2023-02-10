a4a630eebde2174b275cbf8bfd07f5fdbd4b7b46
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-10 23:24:32 +0100

    creating period model

---
+++ models/period.model.js
@@ -0,0 +1,30 @@
+const Period=require('../classes/Country.class');
+const { dbquery } = require('../utils/dbutils');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const addOne=async(period)=>{
+
+}
+
+const updateOne=async(period)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports = {
+    findAll,
+    findOne,
+    addOne,
+    updateOne,
+    deleteOne
+}
