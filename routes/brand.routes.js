11569d5488696967250cdd47f1e395e2e3b41806
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-10 18:14:01 +0100

    adding brand class

--- routes/brand.routes.js
+++ routes/brand.routes.js
@@ -4,8 +4,8 @@ const {userCheck}=require('../middlewares/UserValidation');
 
 router.get('/',brandController.getAll);
 router.get('/:id',brandController.getOne);
-router.post('/',brandController.addOne);
-router.put('/:id',brandController.updateOne);
-router.delete('/:id',brandController.deleteOne);
+router.post('/',userCheck,brandController.addOne);
+router.put('/:id',userCheck,brandController.updateOne);
+router.delete('/:id',userCheck,brandController.deleteOne);
 
 module.exports=router;
