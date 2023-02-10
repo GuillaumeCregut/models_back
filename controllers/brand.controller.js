fa44cfc7c964b7a09d54b7fc3c511883ba76d1e8
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-10 18:00:40 +0100

    adding empty controller and router

---
+++ controllers/brand.controller.js
@@ -0,0 +1,27 @@
+const getAll=()=>{
+
+};
+
+const getOne=()=>{
+
+};
+
+const addOne=()=>{
+
+}
+
+const updateOne=()=>{
+
+}
+
+const deleteOne=()=>{
+
+};
+
+module.exports={
+    getAll,
+    getOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
