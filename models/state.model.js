5b1818180d0dffc153efa7f946bc61ecdfa2c989
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 11:44:31 +0100

    adding state model

---
+++ models/state.model.js
@@ -0,0 +1,30 @@
+const State=require('../classes/State.class');
+const { dbquery } = require('../utils/dbutils');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const addOne=async(state)=>{
+
+}
+
+const updateOne=async(state)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports={
+    findAll,
+    findOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
