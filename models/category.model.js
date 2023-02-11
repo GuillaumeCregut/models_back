883c33cd11c552695d39254f6c8b24e339816170
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 11:18:09 +0100

    adding category model squeleton

---
+++ models/category.model.js
@@ -0,0 +1,30 @@
+const Category=require('../classes/Category.class');
+const { dbquery } = require('../utils/dbutils');
+
+const getAll=async()=>{
+
+}
+
+const getOne=async(id)=>{
+
+}
+
+const addOne=async(category)=>{
+
+}
+
+const updateOne=async(category)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports={
+    getAll,
+    getOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
