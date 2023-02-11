19d8de0fb5380f73ab8d249b7237ebb5c5f4d90f
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 12:40:48 +0100

    testing state route OK

--- routes/state.route.js
+++ routes/state.route.js
@@ -6,6 +6,6 @@ router.get('/',stateController.getAll);
 router.get('/:id',stateController.getOne);
 router.post('/',userCheck,stateController.addOne);
 router.put('/:id',userCheck,stateController.updateOne);
-router.delete('/;id',userCheck,stateController.deleteOne);
+router.delete('/:id',userCheck,stateController.deleteOne);
 
 module.exports=router;
