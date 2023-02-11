dc5e3ddf34f0e3541b028f1ddbba99d78df863af
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 10:45:54 +0100

    adding scale model squeleton

---
+++ models/scale.model.js
@@ -0,0 +1,30 @@
+const Scale=require('../classes/Scale.class');
+const { dbquery } = require('../utils/dbutils');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const addOne=async(scale)=>{
+
+}
+
+const updatOne=async(scale)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports={
+    findAll,
+    findOne,
+    addOne,
+    updatOne,
+    deleteOne,
+}
