544eb83410a21a1880414d3edc20e3932298ccb5
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-12 18:57:46 +0100

    creating controller template for builder

---
+++ controllers/builder.controller.js
@@ -0,0 +1,31 @@
+const Builder=require('../classes/Builder.class');
+const builderModel=require('../models/builder.model');
+const Joi=require('joi');
+
+const getAll=async(req,res)=>{
+
+}
+
+const getOne=async(req,res)=>{
+
+}
+
+const addOne=async(req,res)=>{
+
+}
+
+const updateOne=async(req,res)=>{
+
+}
+
+const deleteOne=async(req,res)=>{
+
+}
+
+module.exports={
+    getAll,
+    getOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
