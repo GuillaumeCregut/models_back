3c7fadf34969b57381858425caa09bb20ac679da
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-12 18:33:15 +0100

    setting userCheck on

--- middlewares/UserValidation.js
+++ middlewares/UserValidation.js
@@ -4,16 +4,16 @@ const userCheck=(req,res,next)=>{
    // For later.
     // const authHeader=req.headers['authorization'];
     // to be relaced by
-    // const authHeader=req.headers.authorization || req.headers.Authorization;
-    // if(!authHeader){ 
-    //     return res.sendStatus(401);
-    // }
-    // const token=authHeader.split(' ')[1];
-    // const isOK=verifyToken(token,'access');
-    // console.log(isOK)
-    // if(!isOK)
-    //     return res.sendStatus(403);
-    // req.user=isOK;
+    const authHeader=req.headers.authorization || req.headers.Authorization;
+    if(!authHeader){ 
+        return res.sendStatus(401);
+    }
+    const token=authHeader.split(' ')[1];
+    const isOK=verifyToken(token,'access');
+    console.log(isOK)
+    if(!isOK)
+        return res.sendStatus(403);
+    req.user=isOK;
     next();
 }
 
