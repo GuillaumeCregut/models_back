3e3574177a7da7af65f4d2cfad389dbd3418fdb8
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-12 18:54:04 +0100

    adding template model for builder

---
+++ models/builder.model.js
@@ -0,0 +1,30 @@
+const { dbquery } = require('../utils/dbutils');
+const Builder=require('../classes/Builder.class');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const addOne=async(builder)=>{
+
+}
+
+const updateOne=async(builder)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports ={
+    findAll,
+    findOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
