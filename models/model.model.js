3710e48427439ec642d21469845eac9fca1a311e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-13 18:17:12 +0100

    adding model

---
+++ models/model.model.js
@@ -0,0 +1,30 @@
+const Model=require('../classes/model.class');
+const { dbquery } = require('../utils/dbutils');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const addOne=async(model)=>{
+
+}
+
+const updateOne=async(model)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports={
+    findAll,
+    findOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
