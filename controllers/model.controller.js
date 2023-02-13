2d997ecac7f881a8edb8d00ab25de0723f387931
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-13 18:20:22 +0100

    adding controller

---
+++ controllers/model.controller.js
@@ -0,0 +1,31 @@
+const Model=require('../classes/model.class');
+const modelModel=require('../models/model.model');
+const Joi=require('joi');
+
+const getAll=async(req,res)=>{
+
+}
+
+const getOne=async(req,res)=>{
+    
+}
+
+const addOne=async(req,res)=>{
+    
+}
+
+const updateOne=async(req,res)=>{
+    
+}
+
+const deleteOne=async(req,res)=>{
+    
+}
+
+module.exports={
+    getAll,
+    getOne,
+    addOne,
+    updateOne,
+    deleteOne,
+}
