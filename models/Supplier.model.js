4a33e58f3dc5b11c39f695a13f87f11e561f73d7
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-25 18:10:03 +0100

    adding supplier model sequeleton

---
+++ models/Supplier.model.js
@@ -0,0 +1,40 @@
+const Supplier =require('../classes/Supplier.class');
+const { dbquery } = require('../utils/dbutils');
+
+const findAll=async()=>{
+
+}
+
+const findOne=async(id)=>{
+
+}
+
+const findAllUser=async(idUser)=>{
+
+}
+
+const findOneUser=async(idUser)=>{
+
+}
+
+const addOne=async(supplier)=>{
+
+}
+
+const updateOne=async(id)=>{
+
+}
+
+const deleteOne=async(id)=>{
+
+}
+
+module.exports={
+    findAll,
+    findAllUser,
+    findOne,
+    findOneUser,
+    addOne,
+    updateOne,
+    deleteOne,
+}
