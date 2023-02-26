8fec6465607150f649f04f81b371179b0698626a
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-26 11:07:35 +0100

    adding test for post order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,7 +1,21 @@
 const Order=require('../classes/Order.class');
 const orderModel=require('../models/order.model');
+const Joi = require('joi');
+
+const validate = (data, option) => {
+    const presence = option ? 'required' : 'optional'
+    return Joi.object({
+        reference: Joi.string().max(200).presence(presence),
+        supplier: Joi.number().integer().presence(presence),
+        owner: Joi.number().integer().presence(presence),
+    }).validate(data, { abortEarly: false }).error;
+}
 
 const getAll=async(req,res)=>{
+
+}
+
+const getAllUser=async(req,res)=>{
 
 }
 
@@ -10,7 +24,13 @@ const getOne=async(req,res)=>{
 }
 
 const addOne=async(req,res)=>{
-
+    //check if datas OK
+    const errors=validate(req.body,true);
+    if (errors){
+        const error=errors.details[0].message;
+        return res.status(422).send(error);
+    }
+    return res.sendStatus(200);
 }
 
 const updateOne=async(req,res)=>{
@@ -27,4 +47,5 @@ module.exports={
     addOne,
     updateOne,
     deleteOne,
+    getAllUser,
 }
--- routes/order.route.js
+++ routes/order.route.js
@@ -3,7 +3,7 @@ const orderController=require('../controllers/order.controller');
 const {userCheck}=require('../middlewares/UserValidation');
 
 router.get('/',orderController.getAll);
-
+router.get('/user/:id',orderController.getAllUser)
 router.get('/:id',orderController.getOne)
 
 router.post('/',orderController.addOne);
