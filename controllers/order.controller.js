99107a39705e3caa6ee4a681dc4a5085e51a782d
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-26 11:44:11 +0100

    validating imputs OK

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,14 +1,30 @@
 const Order=require('../classes/Order.class');
 const orderModel=require('../models/order.model');
 const Joi = require('joi');
+const { required } = require('joi');
 
-const validate = (data, option) => {
-    const presence = option ? 'required' : 'optional'
+const validate = (data, option=true) => {
+    const presence = option ? 'required' : 'optional';
     return Joi.object({
         reference: Joi.string().max(200).presence(presence),
         supplier: Joi.number().integer().presence(presence),
         owner: Joi.number().integer().presence(presence),
     }).validate(data, { abortEarly: false }).error;
+}
+
+const validateList=(data,option=true)=>{
+    const presence = option ? 'required' : 'optional';
+
+    const schema=
+        Joi.object({
+            idModel:Joi.number().integer().required(),
+            price : Joi.number().required(),
+            qtty:Joi.number().integer().required()
+        })
+    return Joi.array().items(schema).validate(data).error;
+    
+    return false;
+    // list:Joi.array().items(Joi.number().integer(),required(),Joi.number().integer().required(),Joi.number().required()).presence(presence)
 }
 
 const getAll=async(req,res)=>{
@@ -25,10 +41,17 @@ const getOne=async(req,res)=>{
 
 const addOne=async(req,res)=>{
     //check if datas OK
-    const errors=validate(req.body,true);
+    const {owner,reference,supplier,list}=req.body;
+    const errors=validate({owner,reference,supplier});
     if (errors){
         const error=errors.details[0].message;
         return res.status(422).send(error);
+    }
+    //check list 
+    const errorsList=validateList(list);
+    if(errorsList){
+        const errorList=errorsList.details[0].message;
+        return res.status(422).send(errorList);
     }
     return res.sendStatus(200);
 }
