123c9557f3c0fd8afb3910a69a2223f299bc9182
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-13 00:58:11 +0200

    secure route

--- routes/stats.route.js
+++ routes/stats.route.js
@@ -2,7 +2,7 @@ const router=require('express').Router();
 const statsController=require('../controllers/stats.controller');
 const {userCheck}=require('../middlewares/UserValidation');
 
-//router.get('/:id',userCheck,statsController.doStats);
-router.get('/:id',statsController.doStats);
+router.get('/:id',userCheck,statsController.doStats);
+//router.get('/:id',statsController.doStats);
 
 module.exports=router;
